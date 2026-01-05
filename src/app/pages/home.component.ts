8acca9c3dff9680f7fcbe5275a7952cabf0a432b
Niranjan <niranjankarunanidhi@gmail.com>
2026-01-05 20:24:00 +0000

    updated hero section

--- src/app/pages/home.component.ts
+++ src/app/pages/home.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   standalone: true,
   selector: 'app-home',
+  imports: [RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
--- src/app/root.component.ts
+++ src/app/root.component.ts
@@ -1,7 +1,6 @@
 import { Component, inject } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet, Router, NavigationEnd } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -14,16 +13,6 @@ export class AppComponent {
   title = 'decor-rentals';
   isMenuOpen = false;
   currentYear = new Date().getFullYear();
-  isEventsPage = false;
-  private router = inject(Router);
-
-  constructor() {
-    this.router.events
-      .pipe(filter(event => event instanceof NavigationEnd))
-      .subscribe((event: any) => {
-        this.isEventsPage = event.urlAfterRedirects === '/events';
-      });
-  }
 
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
