f9a52752ce18d16d41a6ef4b210fea9ded2792fe
Niranjan <niranjankarunanidhi@gmail.com>
2026-01-24 20:56:50 +0000

    fix: Set Router basename for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import Forecast from './pages/Forecast';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <AuthProvider>
         <Routes>
           <Route path="/login" element={<Login />} />
